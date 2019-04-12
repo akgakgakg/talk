a52c58a5433ed1e490cf07aeebf5d565e85f5b9e
snawbel <erik@unsworn.org>
2019-04-12 14:10:55 +0200

    Added params

--- test.js
+++ test.js
@@ -1,17 +1,31 @@
 // Imports the Google Cloud client library
 const fs = require('fs');
 const speech = require('@google-cloud/speech');
+const program = require('commander');
+
 
 // Creates a client
 const client = new speech.SpeechClient();
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-const filename = './providence.wav';
-const encoding = 'LINEAR16'; //'Encoding of the audio file, e.g. LINEAR16';
-const sampleRateHertz = 22050;
-const languageCode = 'en-UK'; //'BCP-47 language code, e.g. en-US';
+program
+  .option('-i, --infile [path]', 'Audio file to be transcribe')
+  .option('-e, --encoding [enc]', 'Audio file encoding: "LINEAR16" | ...  ')
+  .option('-s, --samplerate [rate]', 'Audio file sample rate in hertz: 22050')
+  .option('-l, --language [rate]', 'Language code: "en-UK"')
+  .parse(process.argv);
+
+
+let filename = program.infile || './providence.wav'
+let encoding = program.encoding || 'LINEAR16'
+let sampleRateHertz = program.samplerate || 22050
+let languageCode = program.language || 'en-UK'
+
+console.log('')
+console.log('Audio file:', filename) 
+console.log('Encoding:', encoding) 
+console.log('Sample rate:', sampleRateHertz) 
+console.log('Language code:', languageCode) 
+console.log('')
 
 const config = {
   encoding: encoding,
